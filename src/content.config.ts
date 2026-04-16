690ead9f1cf301a6b7c06105edfb3803b90b2b7b
Saurabh Prakash <saurabhp75@gmail.com>
2026-04-16 19:38:56 +0530

    Use md files instead of json for data.

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,12 @@
-import { file } from "astro/loaders"
+import { glob } from "astro/loaders"
 import { z } from "astro/zod"
 import { defineCollection } from "astro:content"
 
 const books = defineCollection({
-    loader: file('src/content/books.json'),
+    loader: glob({ base: "src/content/books", pattern: "*.md" }),
     schema: z.object({
-        id: z.string(),
+        // id is automatically generated from the file name
+        // id: z.string(), 
         title: z.string(),
         author: z.string(),
         rating: z.number(),
